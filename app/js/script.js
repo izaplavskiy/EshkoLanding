08e2ffce886eba0ea69117147f6c9046b82b0216
Igor <i.zaplavskiy@gmail.com>
2017-11-24 01:42:54 +0200

    Project complete

---
+++ app/js/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+
+$(document).ready(function() {
+
+// style select
+
+    // var params = {
+    //     changedEl: "select",
+    //     visRows: 5,
+    //     scrollArrows: true
+    // }
+    // cuSel(params);
+
+    $( "select" ).selectmenu();
+
+  //   $( "#select-courses" ).click(function() {
+  // alert( "Handler for .click() called." );
+// });
+	
+})
